acd82a91ac4079b405cab5689042420d021577f3
lips99poli <filippo.lipari@mail.polimi.it>
2024-04-26 16:03:44 +0000

    correction of many of the methods in order to correct mistakes in the trials

--- SparseMatrix.hpp
+++ SparseMatrix.hpp
@@ -3,7 +3,12 @@
 
 #include "SparseMatrixTraits.hpp"
 #include <iostream>
+#include <limits>
 
+enum State {
+    COMPRESSED=0,
+    UNCOMPRESSED=1
+};
 
 template<StorageOptions SO, typename T>
 class SparseMatrix{
@@ -18,23 +23,35 @@ class SparseMatrix{
     size_type cols;
     uncompressed_container_type uncompressed_data;
     compressed_container_type compressed_data;
+    State state;
+    T zero = 0.;
+    T buffer = std::numeric_limits<T>::quiet_NaN();;
 
-    static constexpr T zero = 0.; //lo zero in realtà potrebbe essere complesso
+    //static constexpr T zero = 0.; //lo zero in realtà potrebbe essere complesso
 
-    static constexpr StorageOptions otherSO = (SO==RowMajor)? ColumnMajor : RowMajor; //perchè il prof la fa static??
+    static constexpr StorageOptions otherSO = (SO==RowMajor)? ColumnMajor : RowMajor; //perchè il prof la fa static?? se lo tolgo da errore
 
     inline bool in_bound(size_type r, size_type c) const;
+
+    bool verify_presence(size_type r, size_type c) const;
 
     public:
     // Constructor
     SparseMatrix(size_type R = 0, size_type C = 0, const uncompressed_container_type& init = {}):
-    rows(R), cols(C), uncompressed_data(init) {
+    rows(R), cols(C), uncompressed_data(init), state(UNCOMPRESSED){
         if (!init.empty()){
-            bool check = true;
-            for (auto cit=init.begin(); check && cit!=init.end(); ++cit){
-                check = cit->first[0]<=R && cit->first[1]<=C;
+            bool adjusted = false;
+            for (auto cit=init.begin(); cit!=init.end(); ++cit){
+                if (cit->first[0]>=rows){
+                    adjusted = true;
+                    resize(cit->first[0],cols);
+                }
+                if(cit->first[1]>=cols){
+                    adjusted = true;
+                    resize(rows,cit->first[1]);
+                }
             }
-            //assert(check && "Given dimensions are not compatible with the given values");
+            if (adjusted) std::cout << "Dimensions adjusted" << std::endl;
         }
     };
 
@@ -54,14 +71,15 @@ class SparseMatrix{
     // non-const version of call operator
     T& operator()(const size_type r, const size_type c);
 
+    // Delete wrongly added element
+    bool delete_element(const size_type r, const size_type c);
+
 };
 
 
+// PRIVATE METHODS:
 
-
-
-// private methods:
-
+// Verify indexes are in bound
 template<StorageOptions SO, typename T>
 bool SparseMatrix<SO,T>::in_bound(size_type r, size_type c) const {
     bool in_bound = (r<=rows && c<=cols);
@@ -69,14 +87,35 @@ bool SparseMatrix<SO,T>::in_bound(size_type r, size_type c) const {
     return in_bound;
 };
 
-
-// pubblic methods:
-
+// Verify if an element is already present
 template<StorageOptions SO, typename T>
-bool SparseMatrix<SO,T>::is_compressed() const {
-    return compressed_data.empty();
+bool SparseMatrix<SO,T>::verify_presence(size_type r, size_type c) const {
+    
+    typename uncompressed_container_type::key_type position = {r,c};
+
+    bool present = false;
+
+    if (state == UNCOMPRESSED) {
+        present = uncompressed_data.find(position) != uncompressed_data.end();
+        }
+    else{
+        for (size_type i=compressed_data.major_change_index[position[SO]]; !present && i<compressed_data.major_change_index[position[SO]+1]; ++i){
+            present = compressed_data.non_major_index[i] == position[otherSO];
+        }
+    }
+    return present;
 };
 
+
+// PUBBLIC METHODS:
+
+// Verify state
+template<StorageOptions SO, typename T>
+bool SparseMatrix<SO,T>::is_compressed() const {
+    return state;
+};
+
+// Resize
 template<StorageOptions SO, typename T>
 void SparseMatrix<SO,T>::resize(size_type R, size_type C){
     // si potrebbe controllare se ho degli elementi con indici maggiori delle nuove taglie e in quel caso mandare lo user a cagare
@@ -84,6 +123,7 @@ void SparseMatrix<SO,T>::resize(size_type R, size_type C){
     cols = C;
 };
 
+// Compress 
 template<StorageOptions SO, typename T>
 void SparseMatrix<SO,T>::compress(){
     // riserva le dimensioni per i vettori dentro il constainer
@@ -102,13 +142,18 @@ void SparseMatrix<SO,T>::compress(){
     }
     // svuota la mappa
     uncompressed_data.clear();
+
+    // cambia lo stato
+    state = COMPRESSED; 
 };
 
+// Uncompress 
 template<StorageOptions SO, typename T>
 void SparseMatrix<SO,T>::uncompress(){
     uncompressed_container_type new_uncompressed_data;
 
     for(size_type major=0; major<compressed_data.major_change_index.size()-1; ++major){
+
         for(size_type non_major=compressed_data.major_change_index[major]; non_major<compressed_data.major_change_index[major+1]; ++non_major){
             typename uncompressed_container_type::key_type position;
             if (SO == RowMajor) {
@@ -118,58 +163,77 @@ void SparseMatrix<SO,T>::uncompress(){
             }
             new_uncompressed_data[position] = compressed_data.values[non_major];
         }
+
     }
 
     uncompressed_data = std::move(new_uncompressed_data);
     compressed_data.clear();
+
+    // cambia lo stato
+    state = UNCOMPRESSED;
 };
 
-
+// const version of call operator
 template<StorageOptions SO, typename T>
 const T& SparseMatrix<SO,T>::operator() (const size_type r, const size_type c) const {
-    in_bound(r,c);
+    if(in_bound(r,c)){
+        typename uncompressed_container_type::key_type position = {r,c};
 
-    typename uncompressed_container_type::key_type position = {r,c};
+        if (verify_presence(r,c)){
 
-    /*if(is_compressed()){//compressed state
-        for(size_type i=compressed_data.major_change_index[position[SO]]; i<compressed_data.major_change_index[position[SO]+1]; ++i){
-            if (compressed_data.non_major_index == position[otherSO]){
-                return compressed_data.values[i];
+            if(state == UNCOMPRESSED){
+                return uncompressed_data.at(position);
+            }else{
+                for (size_type i=compressed_data.major_change_index[position[SO]]; i<compressed_data.major_change_index[position[SO]+1]; ++i)
+                    if (compressed_data.non_major_index[i] == position[otherSO]) return compressed_data.values[i];
             }
+
+        }else{
+            return 0.;
         }
-
-    }else{//uncompressed state
-        const auto it = uncompressed_data.find(position);
-        if (it != uncompressed_data.end())
-            return it->second;
     }
-
-    // In bound but not present
-    return zero;*/
-    return uncompressed_data.at(position);
+    else return buffer;
 };
-
 
 // non-const version of call operator
 template<StorageOptions SO, typename T>
 T& SparseMatrix<SO,T>::operator()(const size_type r, const size_type c) {
-    in_bound(r,c);
-    typename uncompressed_container_type::key_type position = {r,c};
-    /*
-    if(is_compressed()){
-        //to be completed: devo restituire una reference a values nella posizione in cui inserisco il nuovo elemento.
-        for(size_type i=compressed_data.major_change_index[position[SO]]; i<compressed_data.major_change_index[position[SO]+1]; ++i){
-            if (compressed_data.non_major_index[i] == position[otherSO]){
-                return compressed_data.values[i];
+    if(in_bound(r,c)){
+
+        typename uncompressed_container_type::key_type position = {r,c};
+        if (verify_presence(r,c)){
+            if(state == UNCOMPRESSED) return uncompressed_data[position];
+            else{
+                for (size_type i=compressed_data.major_change_index[position[SO]]; i<compressed_data.major_change_index[position[SO]+1]; ++i)
+                    if (compressed_data.non_major_index[i] == position[otherSO]) return compressed_data.values[i];
+            return zero;
+            }
+        }else{
+            if(state == UNCOMPRESSED){
+                
+                std::cout << "L'elemento in posizione {" << r << "," << c << "} è stato aggiunto" << std::endl;
+                return uncompressed_data[position];
+            }
+            else{
+                return buffer;
             }
         }
-        std::cerr << "In compressed state you can only change values of existing numbers, uncompress your data if you want to change zeros" << std::endl;
-        return zero;
-    }else{
-        return uncompressed_data[position]; //attenzione che se poi lo user non lo cambia T rimane al default che io non ho ancora gestito
     }
-    */
-    return uncompressed_data[position];
+
+    else return buffer;
 };
+
+// Deletion of wrongly added element
+template<StorageOptions SO, typename T>
+bool SparseMatrix<SO,T>::delete_element(const size_type r, const size_type c){
+    if(state == UNCOMPRESSED){
+        uncompressed_data.erase({r,c});
+        return true;
+    }
+    return false;
+};
+
+
+
 
 #endif
